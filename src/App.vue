a2b2b386d7d30c1fc77be2012188e00434ea04fa
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2021-12-30 14:14:52 +0100

    base

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar dense app>
+      <v-app-bar-title v-text="'MolenGeek Shop'"></v-app-bar-title>
+      <v-row>
+        <v-col cols="8">
+          <v-btn rounded small color="blue">Login</v-btn>
+          <v-btn rounded small color="white" class="black--text">Register</v-btn>
+        </v-col>
+        <v-col cols="4"></v-col>
+      </v-row>
+    </v-app-bar>
+    <v-main>
+      <router-view :key="$route.fullPath" />
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
+export default {
+  name: 'App',
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
